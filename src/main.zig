8f8de5f51d82883a4b826ce5b8e94a7985247e17
v3rd3-prely <razvan.mihai.prelipcean@gmail.com>
2026-02-27 20:51:24 +0200

    added filepath as command line argument
    small tweak

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,29 @@
 const std = @import("std");
-const sudoku_solver = @import("sudoku_solver");
+const solver = @import("sudoku_solver");
 
 pub fn main() !void {
-    var table = try sudoku_solver.readTable("extreme.in");
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    const allocator = gpa.allocator();
+    defer switch (gpa.deinit()) {
+        .leak => std.debug.print("Allocator leak.\n", .{}),
+        // .ok => std.debug.print("All good.\n", .{}),
+        .ok => {},
+    };
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+    // _ = &gpa;
+    // for (args, 0..) |arg, i| {
+    //     std.debug.print("{d}: {s}\n", .{ i, arg });
+    // }
+    if (args.len < 2) {
+        std.debug.print("no filepath given\n", .{});
+    }
+    var table = try solver.readTable(args[1]);
     _ = &table;
     var solution: [9][9]u16 = undefined;
 
-    std.debug.print("Solved: {any}\n", .{sudoku_solver.new_solve(&solution, &table)});
+    std.debug.print("Solved: {any}\n", .{solver.new_solve(&solution, &table)});
 
-    sudoku_solver.displayTable(&solution, null);
-    sudoku_solver.displayTable(&table, null);
+    solver.displayTable(&solution, null);
+    solver.displayTable(&table, null);
 }
